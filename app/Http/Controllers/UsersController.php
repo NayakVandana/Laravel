54d9d8f54160126b66aa4a90db1de0495a262880
Vandana Nayak <vandunayak4000@gmail.com>
2023-07-13 11:58:49 +0530

    form_validation

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class UsersController extends Controller
 {
     function getData(Request $req){
+        $req->validate([
+            'username'=>'required | max:10',
+            'userpassword'=>'required | min:5',
+        ]);
         return $req->input();
     }
 
